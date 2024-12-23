66dd04d00f28f95c63cda3bc00acf67cec28f8bc
Cajun Daniell <cdaniell4221@gmail.com>
2024-12-23 21:59:43 +0500

    Add handlers

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello, %s", task)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	body := requestBody{}
+	err := json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		http.Error(w, "JSON должен содержать поле 'message'", http.StatusBadRequest)
+		return
+	}
+	task = body.Message
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/hello", GetHandler).Methods("GET")
+	router.HandleFunc("/api/hello", PostHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
